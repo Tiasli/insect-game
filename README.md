05107c08ecf3aceb59c24adaaa309dbff33bda06
Aslı Tiryaki <tiryakiasl@gmail.com>
2026-02-12 23:14:36 +0300

    added abilities panel

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # bugs-ecosystem
+
+This is a game where the enemy (a human hand) tries to hit and squish you, a simple insect. 
+You try to use whatever tactics your insect have against the hand.
+
+This is an ongoing project where I try to learn about OOP methods.
+Enemy and fight mechanics have yet to be coded. 
+Classes and insect types are updated.
+
+Sources :
+https://www.amentsoc.org/insects/fact-files/orders/ 
--- insect.py
+++ insect.py
@@ -4,6 +4,12 @@ class Insect:
         self.defence = defence
         self.mobility = mobility
         self.hp = hp
+        
+    def abilities(self):
+        print("=== Insect Abilities ===\n")
+        print("1. Protect \n")
+        print("2. Antennea\n")
+        
     
     def protect(self):
         self.defn += 10
@@ -17,6 +23,14 @@ class Insect:
 class Flies(Insect):
     def __init__(self, power, defence, mobility, hp):
         super().__init__(power, defence, mobility, hp)
+        
+    def abilities(self):
+        super().abilities()
+        print("=== Fly Abilities ===\n")
+        print("3. Flying \n")
+        print("4. Acrobatics\n")
+        print("5. Diseases\n")
+        
     
     def flying(self):
         self.mobility += 20
@@ -36,8 +50,13 @@ class Housefly(Flies):
 class Mosquitos(Flies): 
     def __init__(self, power, defence, mobility, hp):
         super().__init__(power, defence, mobility, hp)
-    
-    def sucking(self):
+        
+    def abilities(self):
+        super().abilities()
+        print("=== Mosquito Abilities ===\n")
+        print("6. Sucking Blood \n")
+       
+    def sucking_blood(self):
         self.power += 20
         print("Some flies can suck blood! - +10 power")
         
@@ -46,7 +65,13 @@ class Mosquitos(Flies):
 class Butterflies(Insect):
     def __init__(self, power, defence, mobility, hp):
         super().__init__(power, defence, mobility, hp)
-    
+        
+    def abilities(self):
+        super().abilities()
+        print("=== Butterfly Abilities ===\n")
+        print("3. Metamorphosis \n")
+        print("4. Camouflage\n")
+        
     def metamorphosis(self):
         self.mobility += 20
         print("Butterflies can metamorphose from larvae, into pupae to a winged adult! : +20 mobility")
@@ -61,6 +86,12 @@ class Hymenoptera(Insect):
     def __init__(self, power, defence, mobility, hp):
         super().__init__(power, defence, mobility, hp)
     
+    def abilities(self):
+        super().abilities()
+        print("=== Hymenoptera (Bees&Ants) Abilities ===\n")
+        print("3. Sting \n")
+        
+    
     def sting(self):
         self.power += 20
         print("It can sting! : +20 power")
@@ -69,10 +100,35 @@ class Hymenoptera(Insect):
 class Bees(Hymenoptera):
     def __init__(self, power, defence, mobility, hp):
         super().__init__(power, defence, mobility, hp)
+        
+    def abilities(self):
+        print("=== Bee Abilities ===\n")
+        print("4. Honey\n")
+        print("5. Flying\n")
+        
     
     def honey(self): 
-        print("Bees make honey for : ")
+        print("Bees make honey : ")
+    
+    def flying(self):
+        self.mobility += 20
+        print("Bees can fly : +20 mobility")
+    
         
+    
+class Ants(Hymenoptera):
+    def __init__(self, power, defence, mobility, hp):
+        super().__init__(power, defence, mobility, hp)
+    
+    def abilities(self):
+        super().abilities()
+        print("=== Fly Abilities ===\n")
+        print("4. Colony \n")
+        
+        
+    def colony(self):
+        self.power *= 100
+        print("Ants live in highly organised colonies with milions of colony members : X100 POWER!")
     
     
     
@@ -96,28 +152,31 @@ class Hand:
         insect.mob -= 5
     
     def hit(self, insect):
-        print("Hand attacks you! Try not to get squashed ")
-        
-        
-    
-    
-    
-        
-
-
+        print("Hand attacks you! Try not to get squashed")
 
 
 def main():
     print("Welcome to the fight!!")
     print("Here are todays fighters! : ")
-    print("Housefly / Mosquito ")
-    user_bug = input("Type your character : ")
-    print("A wild hand appears from above. It seems like they try to hit you. How dare they!")
+    print("Housefly / Mosquito / Butterfly / Bee / Ant")
 
+    insect_map = {
+        "housefly" : Housefly,
+        "mosquito" : Mosquitos,
+        "butterfly" : Butterflies,
+        "bee" : Bees,
+        "ant" : Ants
+    }  
+    user_ins = input("Type your character : ").lower()
+    
+    if user_ins in insect_map:
+        player = insect_map[user_ins](power=10, defence=5, mobility=5, hp=20)
+        print(f"You chose {user_ins}!")
+        player.abilities()
+    else:
+        print("Invalid character! Choose from:", list(insect_map.keys()))
+           
         
-        
-        
-
     
 if __name__ == "__main__":
     main()
